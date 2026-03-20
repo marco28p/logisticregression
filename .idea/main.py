92bf29f01f29bc2d808dd957ccc8bacc8c2128da
Marco Petrino <marco3.14p@gmail.com>
2026-03-20 13:30:41 -0400

    working on main file

--- .idea/main.py
+++ .idea/main.py
@@ -1,2 +1,7 @@
-class main:
-    values = []
+#import pandas as pd
+#from pathlib import Path
+
+#class main:
+ #   data = pd.read_cv(Path("/Users/mp/AndroidStudioProjects/logisticregression/.idea/values.csv"))
+  #  print(data.head())
+
