de335bd427b0f6046b198e3493d498727e7163d0
Marco Petrino <marco3.14p@gmail.com>
2026-03-20 14:16:08 -0400

    Refactoring

--- .idea/main.py
+++
@@ -1,7 +0,0 @@
-#import pandas as pd
-#from pathlib import Path
-
-#class main:
- #   data = pd.read_cv(Path("/Users/mp/AndroidStudioProjects/logisticregression/.idea/values.csv"))
-  #  print(data.head())
-
